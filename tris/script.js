c80602a876b515b6705d3f3c1ad64c97dfb5f694
Antonio Ruggiero <antonrugg@gmail.com>
2022-01-26 17:28:48 +0100

    game logic part 2 - tris

--- tris/script.js
+++ tris/script.js
@@ -1,8 +1,63 @@
 const cells = document.querySelectorAll('.cell');
 
-const cell = cells[0];
+let turn = 0;
 
-cell.addEventListener('click', function () {
-    cell.innerText = 'X';
-})
+const cellSigns = [];
 
+for (let i = 0; i < cells.length; i++){
+    const cell = cells[i];
+
+    cell.addEventListener('click', function () {
+
+
+    if (cellSigns[i]) {
+        return;
+    }
+
+    turn++;
+    let sign;
+    if (turn % 2 === 0) {
+        sign = 'O'
+    } else {
+        sign = 'X'
+    }
+
+    
+        cell.innerText = sign;
+        cellSigns[i] = sign;
+
+        let hasWon = checkVictory();
+
+        if (hasWon) {
+            alert(`${sign} ha vinto`)
+        } else if(turn === 9){
+            
+        }
+
+    })
+}
+
+function checkVictory() {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const combination = winningCombinations[i];
+        const a = combination[0];
+        const b = combination[1];
+        const c = combination[2];
+
+        if (cellSigns[a] && cellSigns[a] === cellSigns[b] && cellSigns[b] === cellSigns[c]) {
+            return true;
+        }
+    }
+    return false;
+}
