6362da5896ab67822b63c80ea2cbc6da1c348cb5
Antonio Ruggiero <antonrugg@gmail.com>
2022-02-02 09:15:11 +0100

    added aliens

--- space-invaders/script.js
+++ space-invaders/script.js
@@ -4,13 +4,14 @@ const rxc = size * size;
 const cells = [];
 
 const aliens = [
-    0,1,2,3,,4,5
-
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
 ];
 
 for (let i = 0; i < rxc; i++) {
     const cell = document.createElement('div');
-    cell.innerText = i;
+    // cell.innerText = i;
     cells.push(cell); 
     grid.appendChild(cell);
     
