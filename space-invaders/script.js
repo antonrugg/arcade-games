6ebbba5761f63294afe5cbb8308201bc72310152
Antonio Ruggiero <antonrugg@gmail.com>
2022-02-11 09:29:47 +0100

    added alien moves - added spaceship

--- space-invaders/script.js
+++ space-invaders/script.js
@@ -4,9 +4,8 @@ const rxc = size * size;
 const cells = [];
 
 const aliens = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    0, 1, 2, 3, 4, 5
+    
 ];
 
 for (let i = 0; i < rxc; i++) {
@@ -29,14 +28,52 @@ function removeAliens() {
     }
 }
 
+let step = 1;
+let direction = 'forward';
 function moveAliens() {
+    const leftEdge = aliens[0] % size === 0;
+    const rightEdge = aliens[aliens.length - 1] % size === size - 1;
     removeAliens();
+
+    if (direction === 'forward' && rightEdge) {
+        for (let i = 0; i < aliens.length; i++){
+            aliens[i] = aliens[i] + size;
+            step = -1;
+            direction = 'backward';
+        }
+        
+    }
+
+    if (direction === 'backward' && leftEdge) {
+        for (let i = 0; i < aliens.length; i++) {
+            aliens[i] = aliens[i] + size;
+            step = 1;
+            direction = 'forward';
+        }
+
+    }
+
     for (let i = 0; i < aliens.length; i++){
-        aliens[i] = aliens[i] + 1;
+        aliens[i] = aliens[i] + step;
     }
 
     drawAliens();
     
 }
+
+setInterval(moveAliens, 500);
+
+let spaceshipIdx = 217;
+cells[spaceshipIdx].classList.add('spaceship');
+
+
+function moveSpaceship(event) {
+    if (event.code !== 'Space') return;
+
+    console.log('move', event);
+}
+
+document.addEventListener('keydown', moveSpaceship)
+
 
 moveAliens();
